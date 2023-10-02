05a33ab6bbbe0a1cf0cc091e7dc6e07d45719a38
dahniardd <dahniardvn09@gmail.com>
2023-10-02 09:47:08 +0700

    Pemilihan Percobaan 3

---
+++ PemilihanPercobaan306.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class PemilihanPercobaan306 {
+    public static void main(String[] args){
+        Scanner input06 = new Scanner(System.in);
+
+        double angka1, angka2, hasil;
+        char operator;
+
+        System.out.print("Masukkan angka pertama: ");
+        angka1 = input06.nextDouble();
+        System.out.print("Masukkan angka kedua: ");
+        angka2 = input06.nextDouble();
+        System.out.print("Masukkan operator (+ - * /): ");
+        operator = input06.next().charAt(0);
+
+        switch (operator) {
+            case '+':
+            hasil = angka1 + angka2;
+            System.out.println(angka1 + "+" + angka2 + "=" + hasil);
+            break;
+            case '-':
+            hasil = angka1 - angka2;
+            System.out.println(angka1 + "-" + angka2 + "=" + hasil);
+            break;
+            case '*':
+            hasil = angka1 * angka2;
+            System.out.println(angka1 + "*" + angka2 + "=" + hasil);
+            break;
+            case '/':
+            hasil = angka1 / angka2;
+            System.out.println(angka1 + "/" + angka2 + "=" + hasil);
+            break;
+        }
+    }
+}
